a61b4b2fadc917afdcb37d934bb342d1560e76a7
yungwifi <smerryman@me.com>
2018-06-26 21:40:53 -0400

    Pushing project to github

---
+++ bullseye/AuthorViewController.swift
@@ -0,0 +1,29 @@
+//
+//  AuthorViewController.swift
+//  bullseye
+//
+//  Created by Spencer Merryman on 6/26/18.
+//  Copyright © 2018 Spencer Merryman. All rights reserved.
+//
+
+import UIKit
+
+class AuthorViewController: UIViewController {
+    
+    @IBAction func close () {
+        dismiss(animated: true, completion: nil)
+    }
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+}
--- bullseye/ViewController.swift
+++ bullseye/ViewController.swift
@@ -5,21 +5,94 @@
 //  Created by Spencer Merryman on 6/25/18.
 //  Copyright © 2018 Spencer Merryman. All rights reserved.
 //
-
+//
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var slider: UISlider!
+    @IBOutlet weak var targetLabel: UILabel!
+    @IBOutlet weak var scoreLabel: UILabel!
+    @IBOutlet weak var roundLabel: UILabel!
+    var targetValue = 0
+    var score = 0
+    var round = 0
+    var currentValue = 0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        currentValue = lroundf(slider.value)
+        startNewRound()
     }
-
+    
+    func updateLabels() {
+        targetLabel.text = String(targetValue)
+        scoreLabel.text = String(score)
+        roundLabel.text = String(round)
+    }
+    
+    func startNewRound() {
+        targetValue = 1 + Int(arc4random_uniform(100))
+        currentValue = 50
+        slider.value = Float(currentValue)
+        round += 1
+        updateLabels()
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
+    @IBAction func sliderMoved(_ slider: UISlider) {
+        print("The value of slider is now: \(slider.value)")
+        currentValue = lroundf(slider.value)
+    }
+    
+    @IBAction func resetGame() {
+        score = 0
+        round = 0
+        startNewRound()
+    }
+    
+    @IBAction func showAlert() {
+        
+        let difference = abs(targetValue - currentValue)
+        let points: Int
+        if difference == 0 {
+            points = (100 - difference) + 100
+        } else if difference == 1 {
+            points = (100 - difference) + 50
+        } else {
+            points = 100 - difference
+        }
+        
+        score += points
+        
+        let title: String
+        if difference == 0 {
+            title = "Perfect"
+        } else if difference < 5 {
+            title = "You almost had it!"
+        } else if difference < 10 {
+            title = "Pretty good! "
+        } else {
+            title = "Not even close...."
+        }
+        
+        let message = "You Scored \(points) Points"
+        
+        let alert = UIAlertController(title: title, message: message, preferredStyle: .alert)
+        
+        let action = UIAlertAction(title: "Awesome", style: .default, handler: {
+         action in
+            self.startNewRound()
+        })
+        
+        alert.addAction(action)
+        
+        present(alert, animated: true, completion: nil)
+        
+    }
+    
 }
-
